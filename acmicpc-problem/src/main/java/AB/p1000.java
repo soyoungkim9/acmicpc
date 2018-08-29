2ba6988e64bd95615c0dd9e1bb797233625f2829
soyoungkim9 <soyoung930623@gmail.com>
2018-08-29 20:31:22 +0900

    백준알고리즘 문제 1000번

---
+++ acmicpc-problem/src/main/java/AB/p1000.java
@@ -0,0 +1,15 @@
+package AB;
+
+import java.util.Scanner;
+
+public class p1000 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in); 
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+        
+        sc.close();
+      }
+}

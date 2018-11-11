acf2d650e90982e97ea8dce85d237cd00c551c2c
soyoungkim9 <soyoung930623@gmail.com>
2018-11-11 16:27:10 +0900

    2018. 10. 21

--- acmicpc-problem/src/main/java/math/p1978.java
+++ acmicpc-problem/src/main/java/math/p1978.java
@@ -11,7 +11,7 @@ public class p1978 {
         
         if(n != 2 && n%2 == 0) return false;
         
-        for(int i = 3; i*i <=n; i+=2) {
+        for(int i = 3; i*i <=n; i+=2) { 
             if(n%i == 0) return false;
         }
         
